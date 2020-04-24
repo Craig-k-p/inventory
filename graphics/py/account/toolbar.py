8cedcf04c4617a1a15dd5809a9df545338194730
Craig <freewill@tuta.io>
2020-04-24 09:16:36 -0500

    Added the presentOptions method and added the options (button widgets) attribute

--- graphics/py/account/toolbar.py
+++ graphics/py/account/toolbar.py
@@ -1,13 +1,17 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.properties import ObjectProperty
+from kivy.uix.widget import WidgetException
 
 from resources.utilities import LogMethods
+from resources.inventoryobjects import InventoryObject
 
 
 class Toolbar(BoxLayout, LogMethods):
 
     create_thing_button = ObjectProperty(None)
     create_container_button = ObjectProperty(None)
+    toolbar_right = ObjectProperty(None)
 
     def __init__(self, **kwargs):
         super(Toolbar, self).__init__(**kwargs)
@@ -16,10 +20,31 @@ class Toolbar(BoxLayout, LogMethods):
             file_str=__file__,
             class_str='Toolbar'
         )
+        self.app = None
+
+        self.options = {
+                Button(
+                text='delete',
+                size=('40px', '30px'),
+                size_hint=(None, None)
+            ),
+            Button(
+                text='edit',
+                size=('40px', '30px'),
+                size_hint=(None, None)
+            ),
+            Button(
+                text='move',
+                size=('40px', '30px'),
+                size_hint=(None, None)
+            )
+        }
 
     def checkForScreen(self, screen_str, app):
         '''Make sure the screen_str is correct before we open a popup. Use app to reference the main
            application instance to check which screen we are using'''
+        if not self.app:
+            self.app = app
         self.logDebug('Checking app.sm.current for a match')
         self.logDebug(f'app.sm.current = {app.sm.current}')
         if app.sm.current == 'container':
@@ -27,8 +52,25 @@ class Toolbar(BoxLayout, LogMethods):
         elif app.sm.current == 'account':
             app.buttonPress('createContainerPopup', None, None)
 
-    def logObjects(self, app):
-        '''Call self.getObjects and put them into the log'''
-        self.logInfo(f'Toolbar.logObjects called')
+    def checkSelection(self, app):
+        '''Check if an object is selected and options need to be added to the toolbar'''
 
-        objects = app.getObjects()
+    def presentOptions(self, app):
+        if not self.app:
+            self.app = app
+
+        if InventoryObject.selected != None:
+            try:
+                for widget in self.options:
+                    self.toolbar_right.add_widget(widget)
+                self.options_drawn = True
+            except WidgetException:
+                pass
+
+        else:
+            try:
+                for widget in self.options:
+                    self.toolbar_right.remove_widget(widget)
+                self.options_drawn = False
+            except WidgetException:
+                pass

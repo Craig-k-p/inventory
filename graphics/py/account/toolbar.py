549d32644281a41f2c7d58f1a62958e3b885a16d
Craig <freewill@tuta.io>
2020-11-03 09:47:03 -0600

    toolbar.py     -Added documentation     -Changed the use of InventoryObject.selected to self.app.Selection.get()     -fixed presentOptions method to remove toolbar options when nothing is selected

--- graphics/py/account/toolbar.py
+++ graphics/py/account/toolbar.py
@@ -9,6 +9,7 @@ from resources.inventoryobjects import InventoryObject
 
 class Toolbar(BoxLayout, LogMethods):
 
+    app = ObjectProperty(None)
     create_thing_button = ObjectProperty(None)
     create_container_button = ObjectProperty(None)
     toolbar_right = ObjectProperty(None)
@@ -16,13 +17,9 @@ class Toolbar(BoxLayout, LogMethods):
     def __init__(self, **kwargs):
         super(Toolbar, self).__init__(**kwargs)
 
-        self.__initLog__(
-            file_str=__file__,
-            class_str='Toolbar'
-        )
-        self.app = None
+        self.__initLog__(file_str=__file__, class_str='Toolbar')
 
-        self.options = {
+        self.options = [
                 Button(
                 text='delete',
                 size=('40px', '30px'),
@@ -38,36 +35,34 @@ class Toolbar(BoxLayout, LogMethods):
                 size=('40px', '30px'),
                 size_hint=(None, None)
             )
-        }
+        ]
 
-    def checkForScreen(self, screen_str, app):
+    def checkForScreen(self, screen_str):
         '''Make sure the screen_str is correct before we open a popup. Use app to reference the main
            application instance to check which screen we are using'''
-        if not self.app:
-            self.app = app
         self.logDebug('Checking app.sm.current for a match')
-        self.logDebug(f'app.sm.current = {app.sm.current}')
-        if app.sm.current == 'container':
-            app.buttonPress('createThingPopup', None, None)
-        elif app.sm.current == 'account':
-            app.buttonPress('createContainerPopup', None, None)
+        self.logDebug(f'self.app.sm.current = {self.app.sm.current}')
+        if self.app.sm.current == 'container':
+            self.app.buttonPress('createThingPopup', None, None)
+        elif self.app.sm.current == 'account':
+            self.app.buttonPress('createContainerPopup', None, None)
 
-    def presentOptions(self, app):
-        if not self.app:
-            self.app = app
-
-        if InventoryObject.selected != None:
-            try:
-                for widget in self.options:
+    def presentOptions(self):
+        '''Display toolbar options for selected objects in user's inventory'''
+        # If an object is selected, add the widgets to the toolbar
+        if self.app.Selection.get().getObj() != None:
+            for widget in self.options:
+                if widget not in self.toolbar_right.children:
                     self.toolbar_right.add_widget(widget)
-                self.options_drawn = True
-            except WidgetException as e:
-                self.logDebug(f'EXCEPTION: {e}')
+            self.options_drawn = True
+            # except WidgetException as e:
+            #     self.logDebug(f'EXCEPTION: {e}')
 
+        # If an object isn't selected, remove the widgets from the toolbar
         else:
-            try:
-                for widget in self.options:
+            for widget in self.options:
+                if widget in self.toolbar_right.children:
                     self.toolbar_right.remove_widget(widget)
-                self.options_drawn = False
-            except WidgetException as e:
-                self.logDebug(f'EXCEPTION: {e}')
+            self.options_drawn = False
+                # except WidgetException as e:
+                #     self.logDebug(f'EXCEPTION: {e}')

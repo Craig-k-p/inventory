e961d5b583e28253638af4d1ce51198cc0fcb4c0
Craig <freewill@tuta.io>
2020-11-07 18:20:15 -0600

    Empty line deleted

--- graphics/py/account/thing_view.py
+++ graphics/py/account/thing_view.py
@@ -1,7 +1,6 @@
 from kivy.properties import ObjectProperty
 from kivy.uix.gridlayout import GridLayout
 
-
 class ThingView(GridLayout):
     name = ObjectProperty(None)
     weight = ObjectProperty(None)

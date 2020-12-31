cbe6f52308da2b75d5a8198e91c1dbaaea1fa554
Craig <freewill@tuta.io>
2020-12-30 19:47:29 -0600

    Major changes to the first screen on startup. The user can now load any *.inventory file in the save_data directory. They can also create a new inventory.     -Deleted LoginScreen class     -Deleted CreateAccountScreen class     -Added CenterAnchorLayout class     -Added FileButton class     -Added LoadFileScreen class     -Added CreateFileScreen class
    Shortened __init__ methods and improved class docstrings.

--- graphics/py/pre_auth/screens.py
+++ graphics/py/pre_auth/screens.py
@@ -1,184 +1,125 @@
 import pprint
+
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
 
 from resources.utilities import UtilityMethods, LogMethods
 
 
-class LoginScreen(Screen, UtilityMethods, LogMethods):
-    ''' The window that draws necesary widgets to the screen such as buttons, text inputs,
-        and labels. It inherits from kivy.uix.screenmanager.screen.Screen. It also does
-        the following:
-            - Grabs widgets from the .kv file for access within this class
-            - Check for tab and enter presses on the keyboard
-            - handle button presses
-            - call appropriate functions if buttons are pressed'''
+class CenterAnchorLayout(AnchorLayout):
+    '''FileButton class declared in screens.kv'''
 
-    # Capture the TextInput widgets from main_widgets.kv LoginWindow class
-    account = ObjectProperty(None)
-    pswd = ObjectProperty(None)
 
+class FileButton(Button, LogMethods):
+    '''FileButton class declared in screens.kv'''
+    app = ObjectProperty(None)
     def __init__(self, **kwargs):
-        '''Call WindowKeyboard.__init__() with super().__init__().  Assign other
-           instance variables.'''
+        super(FileButton, self).__init__(**kwargs)
+        self.__initLog__(file_str='screens.py', class_str='FileButton')
 
-        # Allows us to call our own LoginScreen.__init__() without overriding Kivy's Screen.__init__()
-        super(LoginScreen, self).__init__(**kwargs)
+    def selectFile(self):
+        self.app.user_file = self.file
+        self.logDebug(self.file)
+        self.app.createUserScreens()
 
-        # # Allow me to check if the enter key is pressed
-        # Window.bind(on_key_down=self._on_keyboard_down)
 
-        self.__initLog__(
-            file_str='pre_auth_screens',
-            class_str='LoginScreen'
-        )
+class LoadFileScreen(Screen, UtilityMethods, LogMethods):
+    ''' The window that draws necesary widgets to the screen such as buttons, text inputs,
+        and labels. It inherits from kivy.uix.screenmanager.screen.Screen'''
 
-        # Put Kivy widgets into a dictionary for looping/DNRY methods in UtilityMethods class
-        self.widgets = {
-            'TextInputs': {}
-        }
-        self.widgets['TextInputs']['account'] = self.account
-        self.widgets['TextInputs']['pswd'] = self.pswd
-        self.logDebug(f'self.widgets was created:\n{pprint.pformat(self.widgets, indent=4)}')
+    file_button_parent = ObjectProperty(None)
+
+    def __init__(self, app, **kwargs):
+        super(LoadFileScreen, self).__init__(**kwargs)
+        self.__initLog__(file_str='screens.py', class_str='LoadFileScreen')
+        self.app = app
+
+    def fillFileOptions(self):
+        '''Find the inventories and fill them into the file_button_parent widget so the user
+           can select one'''
+
+        files = self.parent.app.getSaveFiles()
+
+        for file in files:
+            layout = CenterAnchorLayout()
+            b = FileButton(text=file)
+            b.on_release = b.selectFile
+            b.file = file
+            on_release = b.selectFile
+            layout.add_widget(b)
+            self.file_button_parent.add_widget(layout)
+            self.logDebug(f'Added {file} button')
+
+
+class CreateFileScreen(Screen, UtilityMethods, LogMethods):
+    ''' The screen that draws necesary widgets to the window such as buttons, text inputs,
+        and labels. It inherits from kivy.uix.screenmanager.screen.Screen'''
+
+    file_name_input = ObjectProperty(None)
+
+    def __init__(self, app, **kwargs):
+        super(CreateFileScreen, self).__init__(**kwargs)
+        self.__initLog__(file_str='pre_auth_screens', class_str='CreateFileScreen')
+        file_name_input = ObjectProperty(None)
+        self.app = app
 
     def checkUserInputFormat(self):
-        '''Check if the user input is valid.  Return user input (tuple) or False'''
+        '''Check if the user input is valid'''
+        file_name = self.file_name_input.text.lower().strip()
 
-        check = True
-        errors = self.manager.app.popup_errors
-        settings = self.manager.app.settings
-        user = self.widgets['TextInputs']['account'].text
-        pw = self.widgets['TextInputs']['pswd'].text
+        self.error = 0
 
-        if check is True:
-            self.logInfo(f'self.checkFormat accepted {self.account.text}')
-            check = (user, pw)
+        errors = []
 
-        return check
+        # Add this error if the filename is too short
+        if len(file_name) < 1:
+            errors.append('File name is blank')
+            self.file_name_input.error = True
+            self.error += 1
+        else:
+            self.file_name_input.error = False
 
-    def initDefaultPopupText(self):
-        '''Set the default popup text messages, popup button prompt, and popup title.  Must be done
-           after the app is instantiated so we can access app.settings.'''
+        # Add this error if the file already exists
+        if self.app.doesFileExist(file_name) == True:
+            errors.append('File already exists')
+            self.file_name_input.error = True
+            self.error += 1
+        else:
+            self.file_name_input.error = False
 
-        # self.popup_text = {}
-        # self.popup_text['messages'] = {
-        #     'invalid login': 'Invalid username/email or password.',
-        #     'Userpass too short': f'Username must be {self.manager.app.settings["username min length"]} and password must be {self.manager.app.settings["password min length"]} characters long',
-        #     'blank login': 'Please provide a username/email and password.',
-        #     'footer': 'Please check and try again.'
-        # }
-        # self.popup_text['button'] = 'Try again'
-        # self.popup_text['title'] = 'Invalid Login'
+        # Add this error if the file contains anything other than underscors and alphanumeric
+        # characters
+        if file_name.replace('_','').isalnum() == False:
+            errors.append('Only alphanumeric characters and underscores allowed.')
+            self.file_name_input.error = True
+            self.error += 1
+        else:
+            self.file_name_input.error = False
 
-        # log = f'Init\'d self.popup_text dictionary:\n{pprint.pformat(self.popup_text, indent=4)}'
-        # self.logDebug(log)
+        # If there are no errors, set the user_file name and load/present the user's data
+        if self.error == 0:
+            self.logDebug('No errors found')
+            self.app.user_file = file_name
+            self.app.createUserScreens()
+        # If there are errors, log them and create a popup to warn the user
+        else:
+            for error in errors:
+                self.logDebug(error)
+            self.app.createPopup(errors=errors)
+            return
 
 
-class CreateAccountScreen(Screen, UtilityMethods, LogMethods):
-    ''' The window that draws necesary widgets to the screen such as buttons, text inputs,
-        and labels. It inherits from kivy.uix.screenmanager.screen.Screen. It also does
-        the following:
-            - Grabs widgets from the .kv file for access within this class
-            - Check for tab and enter presses on the keyboard
-            - handle button presses
-            - call appropriate functions if buttons are pressed'''
 
-    username = ObjectProperty(None)
-    email = ObjectProperty(None)
-    pswd = ObjectProperty(None)
-    pswdrpt = ObjectProperty(None)
-
-    def __init__(self, **kwargs):
-        # Allows us to call our own CreateAccountScreen.__init__() without overriding
-        # Kivy's Screen.__init__()
-        super(CreateAccountScreen, self).__init__(**kwargs)
-
-        # # Allow me to check if the enter key is pressed
-        # Window.bind(on_key_down=self._on_keyboard_down)
-
-        # Creates self.log which saves to app_home_dir/logs/inventory.day.month.year.log
-        self.__initLog__(
-            file_str='pre_auth_screens',
-            class_str='CreateAccountScreen'
-        )
-
-        # Put the Kivy widgets into a dictionary for DNRY/ looping
-        self.widgets = {
-            'TextInputs': {
-                'pswd': self.pswd,
-                'pswdrpt': self.pswdrpt,
-                'email': self.email,
-                'username': self.username
-            }
-        }
-        self.logDebug(f'Created self.widgets:\n{pprint.pformat(self.widgets, indent=4)}')
-
-    def checkUserInputFormat(self):
-        '''Check if the user input is valid.  Return '''
-
-        user = self.username.text
-        email = self.email.text
-        pw = self.pswd.text
-        pw2 = self.pswdrpt.text
-
-        print(f'USER: {self.username.text}, EMAIL:{self.email}')
-
-        check = True
-
-        if len(user) < self.manager.app.settings['username min length']:
-            self.logDebug(f'{user} did NOT pass as valid input')
-            msg = f'{user} is not {self.manager.app.settings["username min length"]} characters long'
-            self.manager.app.popup_errors.append(msg)
-            check = False
-
-        if self.isEmail(email) is False:
-            self.logDebug(f'{email} did NOT pass as valid input')
-            msg = f'"{email}" is not proper email format'
-            self.manager.app.popup_errors.append(msg)
-            check = False
-
-        if self.checkPw(pw, pw2) is False:
-            self.logDebug('Password inputs did NOT pass as valid')
-            msg = 'Passwords do not match or are not '
-            msg += f'{self.manager.app.settings["username min length"]} characters long'
-            self.manager.app.popup_errors.append(msg)
-            check = False
-
-        if check is True:
-            self.logDebug('User create account input passed as valid input in screens.py')
-            check = (user, email, pw)
-
-        return check
-
-    def initDefaultPopupText(self):
-        '''Set the default popup text messages, popup button prompt, and popup title.  Must be done
-           after the app is instantiated so we can access app.settings.'''
-        # self.popup_text = {
-        #     'messages': {
-        #         'invalid username length': 'Username should be at least ' +
-        #                                    f'{self.manager.app.settings["username min length"]} ' +
-        #         'characters long',
-        #         'invalid email': 'The email must be a properly formatted email.',
-        #         'password mismatch': 'The passwords do not match.',
-        #         'password length': f'Your password must be at least ' +
-        #         f'{self.manager.app.settings["password min length"]} characters long',
-        #         'footer': 'Please try again.'
-        #     },
-        #     'button': 'Try again',
-        #     'title': 'Invalid Account Creation Attempt'
-        # }
-        # self.logDebug(
-        #     f'Init\'d self.popup_text dictionary:\n{pprint.pformat(self.popup_text, indent=4)}'
-        # )
 
 
 class InventoryScreenManager(ScreenManager, LogMethods):
-    '''Handles screen changes and holds screen widgets in self.get_current, self.get_screen('name')'''
+    '''Handles screen changes and holds screen widgets in self.get_current,
+       self.get_screen('name')'''
 
     def __init__(self, **kwargs):
         super(InventoryScreenManager, self).__init__(**kwargs)
-        self.__initLog__(
-            file_str='pre_auth_screens',
-            class_str='InventoryScreenManager'
-        )
-        self.logDebug('Instantiated an InventoryScreenManager')
+        self.__initLog__(file_str='pre_auth_screens', class_str='InventoryScreenManager')

a127aa90f100a454213124903b6a1b973d36ef3f
Craig <freewill@tuta.io>
2020-12-30 19:53:15 -0600

    Removed bloat and other minor changes:     -Removed unnecessary comments at the top     -Consolidated comments in the __init__ method     -Removed excessive/redundant debug logs     -Removed unused app settings Other minor changes:     -Changed how the screens are initialy added to self.sm     -Updated the screen names to reflect name changes     -Added a call to lfs.fillFileOptions so the user can choose a file or create a new one

--- inventory.py
+++ inventory.py
@@ -1,19 +1,3 @@
-'''https://www.youtube.com/watch?v=xaYn4XdieCs
-
-Create executable that can be pinned to Start or Task bar:
-
-Resource:
-https://winaero.com/blog/pin-a-batch-file-to-the-start-menu-or-taskbar-in-windows-10/
-
-Create a .bat file with commands:
-    python full_path_to_file
-    cmd /k
-
-Create a shortcut for the .bat file
-Right click and edit target to say:
-    cmd /k already_existing_filepath_that_you_should_not_edit'''
-
-
 from pprint import pformat
 
 from kivy.app import App
@@ -21,7 +5,7 @@ from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.screenmanager import NoTransition, SlideTransition
 
-from graphics.py.pre_auth.screens import LoginScreen, CreateAccountScreen
+from graphics.py.pre_auth.screens import LoadFileScreen, CreateFileScreen
 from graphics.py.pre_auth.screens import InventoryScreenManager
 from graphics.py.account.datagrid import DataGrid, ContainerHeadingRow
 from resources.kv_extensions import KivyExtensions
@@ -36,25 +20,10 @@ class MyInventoryApp(App, KivyExtensions, InventoryHandler, LogMethods):
     def __init__(self, **kwargs):
         super(MyInventoryApp, self).__init__(**kwargs)
         self.__initLog__(file_str='inventory', class_str='MyInventoryApp')
-
-        self.logInfo('Creating MyInventoryApp instance')
-
-        file = 'save_data/save_data.inventory'
-
         # Allow access to the Selection class through the app instance
         self.selection = Selection
-
-        # Error messages here will display to the user
-        self.popup_errors = []
-
         self.settings = {
-            'password min length': 0,
-            'username min length': 0,
-            'start screen': 'login', # account, container, thing
-            'startup transition': NoTransition(),
-            'app transition type': NoTransition(),
-            'save file': file,
-            'backup save file': 'save_data/backup_save_data'
+            'save file path': 'save_data/'
             }
 
         self.kv_settings = {
@@ -71,18 +40,19 @@ class MyInventoryApp(App, KivyExtensions, InventoryHandler, LogMethods):
             'overview options button width': 35,
             'overview weight field width': 60,
             'popup auto_dismiss': False,
-            'popup required fields': ['description', 'usd_value', 'weight'],
             'popup size_hint': (None, None),
             'popup size': (600, 600),
-            'standard font': 'arial',
+            'font': 'arial',
             'text color': (1, 1, 1, 1),
-            'title font name': 'segoesc',}
+            'title font name': 'segoesc',
+            'transition': NoTransition(),
+            }
 
         self.logInfo(f'Started session with settings:\n{pformat(self.settings, indent=4)}')
         self.logInfo(f'Started session with settings:\n{pformat(self.kv_settings, indent=4)}')
 
         # Set window color:  black: (0, 0, 0, 1), white: (1, 1, 1, 1)
-        Window.clearcolor = (0, 0, 0, 1)
+        Window.clearcolor = (0.06, 0.06, 0.06, 1)
 
         # Load the .kv files to start building the GUI
         for file in self.kv_settings['startup kv files']:
@@ -94,34 +64,29 @@ class MyInventoryApp(App, KivyExtensions, InventoryHandler, LogMethods):
         InventoryScreenManager.app = self
 
         # Create a ScreenManager instance with no transition movement
-        self.sm = InventoryScreenManager(transition=self.settings['startup transition'])
+        self.sm = InventoryScreenManager(transition=self.kv_settings['transition'])
         log = 'Created self.sm, instance of InventoryScreenManager with startup transition as '
-        log += f'{self.settings["startup transition"]} --- {self.sm}'
+        log += f"{self.kv_settings['transition']} --- {self.sm}"
         self.logDebug(log)
 
-        screens = [
-            # These are added to the screen manager sm so that the screen manager sm
-            # name='name' allows the screen manager to change screens using self.current='screen'
-            CreateAccountScreen(name='create account'),
-            LoginScreen(name='login'),
-            # name can also be defined in the kv/main_widgets.kv file using "name: 'account window'" under
-            # class declaration
-        ]
 
-        log = f'Created login, account, and create account screens with "screens" var: '
-        log += f'{pformat(screens, indent=4)}'
-        self.logDebug(log)
+        # name='name' allows the screen manager to change screens using self.current='screen'
+        # name can also be defined in the kv/main_widgets.kv file using "name: 'load file'" under
+        # class declaration
+        lfs = LoadFileScreen(self, name='load file')
+        cas = CreateFileScreen(self, name='create file')
+        screens = [lfs, cas]
 
         # Make sure sm knows how to handle changes to self.current
-        # such as self.current = 'account window'
+        # such as self.current = 'account screen'
         for screen in screens:
             self.sm.add_widget(screen)
-        self.logDebug('Added the screens to self.sm InventoryScreenManager instance')
 
-        # Set the current screen to login window
-        self.sm.current = self.settings['start screen']
-        self.sm.transition = self.settings['app transition type']
-        self.logDebug('Set the first screen to settings["start screen"]')
+        lfs.fillFileOptions()
+
+        # Set the current screen
+        self.sm.current = 'load file'
+        self.sm.transition = self.kv_settings['transition']
 
     def build(self):
         '''Without returning self.sm, the app would be a blank screen.'''
@@ -131,10 +96,8 @@ class MyInventoryApp(App, KivyExtensions, InventoryHandler, LogMethods):
 
     def on_stop(self):
         '''Execute this function when the application is exited'''
+        self.logInfo(f'self.inventoryobject.changes_made = {self.inventoryobject.wasChangeMade()}')
         self.saveData()
-        self.logDebug(f'self.inventoryobject.changes_made = {self.inventoryobject.wasChangeMade()}')
-        self.logDebug(f'InventoryObject.changes_made = {InventoryObject.wasChangeMade()}')
-        self.logDebug('Shutting down..')
 
 
 if __name__ == '__main__':

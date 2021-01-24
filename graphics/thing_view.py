27a118ab8680edb425b5aa8346a181d588b2f6b0
Craig <freewill@tuta.io>
2021-01-24 10:56:57 -0600

    Delete thing_view.kv and .py files

--- graphics/thing_view.py
+++
@@ -1,8 +0,0 @@
-from kivy.properties import ObjectProperty
-from kivy.uix.gridlayout import GridLayout
-
-class ThingView(GridLayout):
-    name = ObjectProperty(None)
-    weight = ObjectProperty(None)
-    val = ObjectProperty(None)
-    tags = ObjectProperty(None)
